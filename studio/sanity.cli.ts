f933aefa268bed941ad2c4f33a42b2003dfddcbe
npinsley <nathan.pinsley@gmail.com>
2026-02-23 13:29:17 -0500

    try sanity self host

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,26 +1,9 @@
-/**
- * Sanity CLI Configuration
- * This file configures the Sanity CLI tool with project-specific settings
- * and customizes the Vite bundler configuration.
- * Learn more: https://www.sanity.io/docs/cli
- */
-
-import {defineCliConfig} from 'sanity/cli'
-
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
-const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
+typescriptimport { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId,
-    dataset,
+    projectId: 'tg4ta723',
+    dataset: 'production',
   },
-  studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/studio/environment-variables to learn more about using environment variables for local & production.
-  autoUpdates: true,
-  typegen: {
-    path: './src/**/*.{ts,tsx,js,jsx}',
-    schema: '../sanity.schema.json',
-    generates: './sanity.types.ts',
-    overloadClientMethods: true,
-  },
+  studioHost: 'trailblazers-park',
 })
