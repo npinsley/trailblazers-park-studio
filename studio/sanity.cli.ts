d9f1995242ef771ca9ae0ea30ad08be0ddb1c7c0
npinsley <nathan.pinsley@gmail.com>
2026-02-23 14:36:01 -0500

    Update sanity.cli.ts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'Ytg4ta723',
+    projectId: 'tg4ta723',
     dataset: 'production',
   },
   studioHost: 'trailblazers-park',
